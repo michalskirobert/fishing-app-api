dbf666d58d02daa704ff053f081ef851fb3ce09d
Robert <majointa@gmail.com>
2024-07-03 18:30:31 +0200

    fix sign-up

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,37 +1,31 @@
-// src/controllers/authController.ts
 import { Request, Response } from "express";
-import { MongoClient } from "mongodb";
+import { connectDatabase } from "@config/database";
+
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import env from "@config/env";
 
-const client = new MongoClient(env.DB_URI);
-
-export async function connectDatabase(name: string) {
-  try {
-    const resp = await client.connect();
-    return resp.db(name);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 export const signUp = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { email, password } = req.body;
-    const db = await connectDatabase("usersDB");
+    const { email, password, permitNo, pesel, birthDate } = req.body;
+    const db = await connectDatabase("auth");
     const usersCollection = db?.collection("users");
 
     const existingUser = await usersCollection?.findOne({ email });
+
     if (existingUser) {
       res.status(400).json({ message: "User already exists" });
       return;
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
+
     const result = await usersCollection?.insertOne({
       email,
       password: hashedPassword,
+      permitNo,
+      pesel,
+      birthDate,
     });
 
     if (result?.insertedId) {
@@ -40,28 +34,29 @@ export const signUp = async (req: Request, res: Response): Promise<void> => {
       res.status(500).json({ message: "User creation failed" });
     }
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message:
-          "Ogólny błąd serwera, proszę skontaktować się z administratorem",
-      });
+    res.status(500).json({
+      message: "Ogólny błąd serwera, proszę skontaktować się z administratorem",
+    });
   }
 };
 
 export const signIn = async (req: Request, res: Response): Promise<void> => {
   try {
     const { email, password } = req.body;
-    const db = await connectDatabase("usersDB");
+    const db = await connectDatabase("auth");
     const usersCollection = db?.collection("users");
 
     const user = await usersCollection?.findOne({ email });
+
+    console.log(user);
+
     if (!user) {
       res.status(400).json({ message: "Invalid email or password" });
       return;
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
+
     if (!isPasswordValid) {
       res.status(400).json({ message: "Invalid email or password" });
       return;
@@ -77,11 +72,8 @@ export const signIn = async (req: Request, res: Response): Promise<void> => {
 
     res.status(200).json({ token });
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message:
-          "Ogólny błąd serwera, proszę skontaktować się z administratorem",
-      });
+    res.status(500).json({
+      message: "Ogólny błąd serwera, proszę skontaktować się z administratorem",
+    });
   }
 };
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import fishingSpotsRoutes from "@routes/fishingSpotsRoutes";
 import authRoutes from "@routes/authRoutes";
 import { authMiddleware } from "@middlewares/authMiddleware";
 
+import cors from "cors";
 import env from "./config/env";
 
 const app = express();
 const port = env.PORT;
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use("/api/fishing-spots", authMiddleware, fishingSpotsRoutes);
 app.use("/api/authentication", authRoutes);
